c446f07f4fecf16ddc8e9ff49238e7e1850417df
Manikandan235 <manikandanramachandran235@gmail.com>
2026-02-24 23:37:25 +0530

    Create EMP table and insert initial data

--- 03.sql
+++ 03.sql
@@ -1,3 +1,7 @@
+Create table EMP(EMPNO number(4) primary key,Ename varchar(10),JOB Varchar(9), MGR number(4),Hiredate date,SAL number(7,2),Comm Number(7,2),Deptno number(2));
+
+insert into EMP values(7369,'SMITH','CLERK',7902,'17-Dec-80',800,null,20);
+
 select SYSDATE from dual;
 
 SELECT EMPNO,ENAME,SAL, ROUND(sal * 1.15, 0) "New Salary" FROM emp;
