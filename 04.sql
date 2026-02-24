44afe59f8a0896ad8def6388dd78c0294d4d396e
Manikandan235 <manikandanramachandran235@gmail.com>
2026-02-24 23:38:06 +0530

    Create EMP table and insert initial employee data
    Created a table for employee records and inserted initial data.

--- 04.sql
+++ 04.sql
@@ -1,3 +1,7 @@
+Create table EMP(EMPNO number(4) primary key,Ename varchar(10),JOB Varchar(9), MGR number(4),Hiredate date,SAL number(7,2),Comm Number(7,2),Deptno number(2));
+
+insert into EMP values(7369,'SMITH','CLERK',7902,'17-Dec-80',800,null,20);
+
 SELECT COUNT(*) FROM EMP;
 
 SELECT MIN(SAL),MAX(SAL),AVG(SAL),SUM(SAL) FROM EMP;
